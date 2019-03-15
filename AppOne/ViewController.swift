239c309d557067ce1ff12ebde46fd22f04612c1c
Elle Elisa Ivantšikova <elleelisa@users.noreply.github.com>
2019-03-15 19:15:23 +0200

    Creating project and simple objects
    Added an UIImageView object, UIView object and some Label objects. Also tested out the constraints and the different views on different devices

---
+++ AppOne/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppOne
+//
+//  Created by Elle Elisa Ivantsikova on 15/03/2019.
+//  Copyright © 2019 Elle Elisa Ivantsikova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
